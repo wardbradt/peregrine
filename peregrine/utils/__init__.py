9dfaef0bae457b5830ff4f8443ec4955a5d6394b
wardbradt <bbradt@andover.edu>
2018-02-27 17:09:23 -0500

    fixed import statements

--- peregrine/utils/__init__.py
+++ peregrine/utils/__init__.py
@@ -1,4 +1,4 @@
-from .drawing import draw_graph_to_file
+from .drawing import draw_graph_to_png
 from .general import ExchangeNotInCollectionsError, get_exchanges_for_market, print_profit_opportunity_for_path
 from .multi_exchange import create_multi_exchange_graph, create_weighted_multi_exchange_digraph, \
     multi_graph_to_log_graph
