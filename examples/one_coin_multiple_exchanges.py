b13c2ebf0777fff1c8c8c8f0beed92d0a7e8775d
wardbradt <bbradt@andover.edu>
2018-03-13 18:34:08 -0400

    fixed one example

--- examples/one_coin_multiple_exchanges.py
+++ examples/one_coin_multiple_exchanges.py
@@ -1,11 +1,11 @@
-from peregrine import create_weighted_multi_exchange_digraph, bellman_ford_multi, \
-    print_profit_opportunity_for_path_multi, calculate_profit_for_path_multi
+from peregrine import create_weighted_multi_exchange_digraph, bellman_ford_multi, calculate_profit_ratio_for_path, \
+    print_profit_opportunity_for_path
 
 
 graph = create_weighted_multi_exchange_digraph(['bittrex', 'gemini', 'kraken'], log=True)
 
 graph, paths = bellman_ford_multi(graph, 'BTC')
 for path in paths:
-    total = calculate_profit_for_path_multi(graph, path)
+    total = calculate_profit_ratio_for_path(graph, path)
     print(path)
-    print_profit_opportunity_for_path_multi(graph, path)
+    print_profit_opportunity_for_path(graph, path)
