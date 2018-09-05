d0c9947b9cb12093b3328ba840d1f2237a09c3e1
wardbradt <bbradt@andover.edu>
2018-09-05 12:51:05 -0400

    added functions for creating graphs from cryptosockets data

---
+++ peregrinearb/utils/wss_graph_builder.py
@@ -0,0 +1,28 @@
+import logging
+import math
+
+
+adapter = logging.getLogger(__name__)
+
+
+def wss_add_market(graph, symbol, market_data):
+    base, quote = symbol.split('/')
+    graph.add_edge(base, quote, weight=float('Inf'), depth=float('Inf'), market_name=symbol, fee=market_data['fee'],
+                   volume=0, no_fee_rate=None, trade_type='SELL')
+    graph.add_edge(quote, base, weight=float('Inf'), depth=float('Inf'), market_name=symbol, fee=market_data['fee'],
+                   volume=0, no_fee_rate=None, trade_type='BUY')
+
+
+def wss_update_graph(graph, symbol, side, price, volume, *args):
+    base, quote = symbol.split('/')
+    fee_scalar = 1 - graph[base][quote]['fee']
+    if side == 'sell':
+        graph[base][quote]['weight'] = -math.log(price * fee_scalar)
+        graph[base][quote]['depth'] = -math.log(volume)
+        graph[base][quote]['volume'] = volume
+        graph[base][quote]['no_fee_rate'] = price
+    else:
+        graph[quote][base]['weight'] = -math.log(fee_scalar * 1 / price)
+        graph[quote][base]['depth'] = -math.log(volume * price)
+        graph[quote][base]['volume'] = volume
+        graph[quote][base]['no_fee_rate'] = price
