4ed0065f290e740a26cfab69a76e792605f5c49c
wardbradt <bbradt@andover.edu>
2018-09-06 11:46:53 -0400

    modified wss_update_graph

--- peregrinearb/utils/wss_graph_builder.py
+++ peregrinearb/utils/wss_graph_builder.py
@@ -23,12 +23,16 @@ def wss_update_graph(graph, symbol, side, price, volume, *args):
     base, quote = symbol.split('/')
     fee_scalar = 1 - graph[base][quote]['fee']
     if side == 'sell':
+        opp_could_exist = price > graph[base][quote]['no_fee_rate']
         graph[base][quote]['weight'] = -math.log(price * fee_scalar)
         graph[base][quote]['depth'] = -math.log(volume)
         graph[base][quote]['volume'] = volume
         graph[base][quote]['no_fee_rate'] = price
     else:
+        opp_could_exist = price < graph[base][quote]['no_fee_rate']
         graph[quote][base]['weight'] = -math.log(fee_scalar * 1 / price)
         graph[quote][base]['depth'] = -math.log(volume * price)
         graph[quote][base]['volume'] = volume
         graph[quote][base]['no_fee_rate'] = price
+
+    return opp_could_exist
