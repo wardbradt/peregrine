86e85c38506f1a8032659abe45a31ad9e67392b2
wardbradt <bbradt@andover.edu>
2018-09-11 19:06:18 -0400

    fixed KeyError bug in wss_update_graph

--- peregrinearb/utils/wss_graph_builder.py
+++ peregrinearb/utils/wss_graph_builder.py
@@ -13,9 +13,9 @@ adapter = logging.getLogger(__name__)
 
 def wss_add_market(graph, symbol, market_data):
     base, quote = symbol.split('/')
-    graph.add_edge(base, quote, weight=float('Inf'), depth=float('Inf'), market_name=symbol, fee=market_data['fee'],
+    graph.add_edge(base, quote, weight=float('Inf'), depth=float('Inf'), market_name=symbol, fee=market_data['taker_fee'],
                    volume=0, no_fee_rate=None, trade_type='SELL')
-    graph.add_edge(quote, base, weight=float('Inf'), depth=float('Inf'), market_name=symbol, fee=market_data['fee'],
+    graph.add_edge(quote, base, weight=float('Inf'), depth=float('Inf'), market_name=symbol, fee=market_data['taker_fee'],
                    volume=0, no_fee_rate=None, trade_type='BUY')
 
 
