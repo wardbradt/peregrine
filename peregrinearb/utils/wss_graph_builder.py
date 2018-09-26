f199bebd494cf6cbb5528b7dcaff53e509c1f695
wardbradt <bbradt@andover.edu>
2018-09-26 17:30:05 -0400

    fixed bug in wss_update_graph

--- peregrinearb/utils/wss_graph_builder.py
+++ peregrinearb/utils/wss_graph_builder.py
@@ -22,17 +22,19 @@ def wss_add_market(graph, symbol, market_data):
 def wss_update_graph(graph, symbol, side, price, volume, *args):
     base, quote = symbol.split('/')
     fee_scalar = 1 - graph[base][quote]['fee']
+    # if the ask was updated
     if side == 'sell':
+        opp_could_exist = price < graph[quote][base]['no_fee_rate']
+        graph[quote][base]['weight'] = -math.log(fee_scalar * 1 / price)
+        graph[quote][base]['depth'] = -math.log(volume * price)
+        graph[quote][base]['volume'] = volume
+        graph[quote][base]['no_fee_rate'] = price
+    # if the bid was updated
+    else:
         opp_could_exist = price > graph[base][quote]['no_fee_rate']
         graph[base][quote]['weight'] = -math.log(price * fee_scalar)
         graph[base][quote]['depth'] = -math.log(volume)
         graph[base][quote]['volume'] = volume
         graph[base][quote]['no_fee_rate'] = price
-    else:
-        opp_could_exist = price < graph[base][quote]['no_fee_rate']
-        graph[quote][base]['weight'] = -math.log(fee_scalar * 1 / price)
-        graph[quote][base]['depth'] = -math.log(volume * price)
-        graph[quote][base]['volume'] = volume
-        graph[quote][base]['no_fee_rate'] = price
 
     return opp_could_exist
