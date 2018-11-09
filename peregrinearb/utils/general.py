93f0fb807dfcc11589a1478a5078b01576431376
wardbradt <bbradt@andover.edu>
2018-11-09 10:20:23 -0500

    fixed spacing

--- peregrinearb/utils/general.py
+++ peregrinearb/utils/general.py
@@ -48,36 +48,32 @@ def print_profit_opportunity_for_path(graph, path, round_to=None, depth=False, s
     if not path:
         return
 
-    if depth:
-        starting_amount = min(float(starting_amount), math.exp(-path['minimum']))
-        path = path['loop']
-
     print("Starting with {} in {}".format(starting_amount, path[0]))
 
     for i in range(len(path) - 1):
-            start = path[i]
-            end = path[i + 1]
+        start = path[i]
+        end = path[i + 1]
 
-            if depth:
-                volume = min(starting_amount, math.exp(-graph[start][end]['depth']))
-                starting_amount = math.exp(-graph[start][end]['weight']) * volume
-            else:
-                starting_amount *= math.exp(-graph[start][end]['weight'])
+        if depth:
+            volume = min(starting_amount, math.exp(-graph[start][end]['depth']))
+            starting_amount = math.exp(-graph[start][end]['weight']) * volume
+        else:
+            starting_amount *= math.exp(-graph[start][end]['weight'])
 
-            if round_to is None:
-                rate = math.exp(-graph[start][end]['weight'])
-                resulting_amount = starting_amount
-            else:
-                rate = round(math.exp(-graph[start][end]['weight']), round_to)
-                resulting_amount = round(starting_amount, round_to)
+        if round_to is None:
+            rate = math.exp(-graph[start][end]['weight'])
+            resulting_amount = starting_amount
+        else:
+            rate = round(math.exp(-graph[start][end]['weight']), round_to)
+            resulting_amount = round(starting_amount, round_to)
 
-            printed_line = "{} to {} at {} = {}".format(start, end, rate, resulting_amount)
+        printed_line = "{} to {} at {} = {}".format(start, end, rate, resulting_amount)
 
-            # todo: add a round_to option for depth
-            if depth:
-                printed_line += " with {} of {} traded".format(volume, start)
+        # todo: add a round_to option for depth
+        if depth:
+            printed_line += " with {} of {} traded".format(volume, start)
 
-            print(printed_line)
+        print(printed_line)
 
 
 def print_profit_opportunity_for_path_multi(graph: nx.Graph, path, print_output=True, round_to=None, shorten=False):
