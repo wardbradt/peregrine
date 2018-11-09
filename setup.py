e84971bae80aa2c03242a54f8451af0c1a3c4aa5
wardbradt <bbradt@andover.edu>
2018-11-09 10:21:59 -0500

    updated version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='peregrinearb',
-    version='1.6.17-0.0.1',
+    version='1.6.18-0.0.1',
     description='A Python library which provides several algorithms to detect arbitrage opportunities across over 90 '
                 'cryptocurrency markets in 34 countries',
     author='Ward Bradt',
